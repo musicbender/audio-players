69e27a81cc968907ed845160a1f8adeaeb9cfdf4
musicbender <musicbender@gmail.com>
2016-04-12 22:40:08 -0700

    started laying down groundwork and first two sections

--- js/main.js
+++ js/main.js
@@ -1,231 +1,100 @@
+$(document).ready(function(){
 
-var main = function () {
+    var context = new (window.AudioContext || window.webkitAudioContext)(),
+        playSound = undefined;
+    
+    function audioFileLoader(fileDirectory) {
+        var soundObj = {};
+        soundObj.fileDirectory = fileDirectory;
+/*var*/ playSound = undefined;
+        var getSound = new XMLHttpRequest();
+        getSound.open("GET", soundObj.fileDirectory, true);
+        getSound.responseType = "arraybuffer";
+        getSound.onload = function() {
+            context.decodeAudioData(getSound.response, function(buffer) {
+                soundObj.soundToPlay = buffer;
+            });
+        }
 
-   //FORM VALIDATION----------------------------------------------------
-   function validate(input1, input2, input3, input4, button, formClass) {
-       //runs valid() function when arrows are clicked
-       formClass.change(function() {
-           valid();
-       });
-       //runs valid() function when anything is typed into the form
-       formClass.keyup(function() {
-           valid();
-       });
-       //validation function
-       function valid() {
-           if ((input3 == null) && (input4 == null)) {
-               if ((input1.val() == "") || (input2.val() == "")) {
-                    button.removeAttr("disabled");
-                    button.attr("disabled", true);
-               } else {
-                   button.removeAttr("disabled");
-               }
-           } else if ((input3 !== null) && (input4 == null)) {
-               if ((input1.val() == "") || (input2.val() == "") || (input3.val() == "")) {
-                    button.removeAttr("disabled");
-                    button.attr("disabled", true);
-               } else {
-                   button.removeAttr("disabled");
-               }
-           } else {
-               if ((input1.val() == "") || (input2.val() == "") || (input3.val() == "") || (input4.val() == "")) {
-                    button.removeAttr("disabled");
-                    button.attr("disabled", true);
-               } else {
-                   button.removeAttr("disabled");
-               }
-           }
-       }
-   }
+        getSound.send();
 
-    //running each validate() function for each form section when page opens
-    validate($('#coffeeDose'), $('#waterDose'), null, null, $('#submitBrewCal'), $('.brew-form'));
-    validate($('#tdsPercent'), $('#dryMass'), $('#brewMass'), null, $('#submitExtCal'), $('.ext-form'));
-    validate($('#preWeight'), $('#postWeight'), null, null, $('#submitLossCal'), $('.loss-form'));
-    validate($('#devMin'), $('#devSec'), $('#totalMin'), $('#totalSec'), $('#submitDevCal'), $('.dev-form'));
+        soundObj.play = function() {
+            playSound = context.createBufferSource();
+            playSound.buffer = soundObj.soundToPlay;
+            playSound.playbackRate.value = (Math.random() * (1.04 - 0.96) + 0.96); //random pitch
+            playSound.connect(context.destination)
+            playSound.start(context.currentTime)
+        }
 
-    //DISABLE COPY/PASTE------------------------------------------------
-    $('.form-control').bind('cut copy paste taphold contextmenu', function(e) {
-    e.preventDefault();
-    });
+        soundObj.stop = function() {
+            playSound.stop(context.currentTime)
+        }
+        
+        return soundObj;
+    };
 
-    //UNITS AND UNITCONVERSION-------------------------------------------
-
-    //FIND UNIT OF NEARBY INPUT BOX
-    function findUnitFromInput(id) {
-        return $(id).siblings().children(".dropdown-toggle:first-child").text();
+    //loop through list of audio files
+    function audioBatchLoader(obj) {
+        for (prop in obj) {
+            obj[prop] = audioFileLoader(obj[prop])
+        }
+        return obj
     }
 
-    //UNIT CONVERSION
-    function unitConvert(value, unit, prev) {
-        unit = unit.trim();
-        prev = prev.trim();
-        if (prev === "grams") {
-            if (unit === "grams") {
-                return value;
-            } else if (unit === "ounces") {
-                return Math.round((parseFloat(value) * 0.035274) * 100) / 100;
-            } else if (unit === "tbsp.") {
-                return Math.round((parseFloat(value) * 0.18867925) * 100) / 100;
-            } else if (unit === "lbs.") {
-                return Math.round((parseFloat(value) / 453.592) * 100) / 100;
-            } else {
-                return 000;
-            }
-        } else if (prev === "ounces") {
-            if (unit === "grams") {
-                return Math.round((parseFloat(value) / 0.035274) * 100) / 100;
-            } else if (unit === "ounces") {
-                return value;
-            } else if (unit === "tbsp.") {
-                return Math.round((parseFloat(value) / 0.19000570) * 100) / 100;
-            } else if (unit === "lbs.") {
-                return Math.round((parseFloat(value) / 16) * 100) / 100;
-            } else {
-                return 000;
-            }
-        } else if (prev === "tbsp."){
-            if (unit === "grams") {
-                return Math.round((parseFloat(value) / 0.18867925) * 100) / 100;
-            } else if (unit === "ounces") {
-                return Math.round((parseFloat(value) * 0.19000570) * 100) / 100;
-            } else if (unit === "tbsp.") {
-                return value;
-            } else {
-                return 000;
-            }
-        } else if (prev === "lbs.") {
-            if (unit === "grams") {
-                return Math.round((parseFloat(value) * 453.592) * 100) / 100;
-            } else if (unit === "ounces") {
-                return Math.round((parseFloat(value) * 16) * 100) / 100;
-            } else if (unit === "lbs.") {
-                return value;
-            }
+    //batch audio loader
+     var sound = audioBatchLoader({
+       //audio files here
+     });
+    
+    //Random number between 1 and 3 for Round Robin
+    function randomNum () {
+        var num = Math.floor(Math.random() * 3);
+        return num;
+    }
+
+    //Play sound with Round Robin
+    function roundRobinPlay (sound1, sound2, sound3, trigger) {
+        var sounds = [sound1, sound2, sound3],
+            rr = randomNum();
+        
+        if ((sound2 !== null) && (sound3 !== null)){
+            sounds[rr].play(context.currentTime);
+            trigger.addClass('hit');
         } else {
-            return 000;
+            sound1.play(context.currentTime);
+            trigger.addClass('hit');
         }
     }
-
-    //RECONVERTS BACK TO GRAMS
-    function reconvertGrams(unit, value){
-        unit = unit.trim();
-        if (unit === "grams") {
-            return value;
-        } else if (unit === "ounces") {
-            return Math.round((parseFloat(value) / 0.035274) * 100) / 100;
-        } else if (unit === "tbsp.") {
-            return Math.round((parseFloat(value) / 0.18867925) * 100) / 100;
-        } else if (unit === "lbs.") {
-            return Math.round((parseFloat(value) * 453.592) * 100) / 100;
+    
+    /*-----Trigger sounds-----*/
+    
+    //Trigger with mouse click.
+    $('.trigger').mousedown(function(){
+        if ($(this).hasClass('boom')){
+            roundRobinPlay(sound.boom, null, null, $('.boom'));
+        } else if ($(this).hasClass('smack')){
+            roundRobinPlay(sound.smack1, sound.smack2, sound.smack3, $('.smack'));
+        } else if ($(this).hasClass('tsst')){
+            roundRobinPlay(sound.tsst1, sound.tsst2, sound.tsst3, $('.tsst'));
         } else {
-            return 000;
+            console.log('ERROR');
         }
-    }
-
-    //STORE PREVIOUS UNIT
-            //will always happen before clicking on a dropdown item.
-    $('.dropdown-toggle').click(function(){
-        $(this).data("prev", $(this).text());
     });
-
-    //DROPDOWN BUTTON ADDON--------------------------------
-    $(".dropdown-menu li a").click(function(e) {
-        e.preventDefault();
-        //changes button text to whatever unit you clicked on
-        $(this).parent().parent().siblings(".dropdown-toggle:first-child").html($(this).text()+' <span class="caret"></span>');
-        $(this).parent().parent().siblings(".dropdown-toggle:first-child").val($(this).text());
-
-        var currentUnit = $(this).text(),
-            currentValue = $(this).parent().parent().parent().siblings().val(), //value in box
-            oldUnit = $(this).parent().parent().siblings().data("prev"), //previous button value
-            convert = unitConvert(currentValue, currentUnit, oldUnit); //converts the value of the textbox
-
-        $(this).parent().parent().parent().siblings().val(convert);
+    
+    $('.trigger').mouseup(function(){
+        $('.trigger').removeClass('hit');
     });
-
-    //CALCULATORS-------------------------------------------------
-
-    //BREW CALCULATOR
-    $('#brewCalForm').submit(function() {
-        var coffeeValue = reconvertGrams(findUnitFromInput('#coffeeDose'), $('#coffeeDose').val()),
-            waterValue = reconvertGrams(findUnitFromInput('#waterDose'), $('#waterDose').val());
-
-        function findRatio (coffee, water) {
-            return Math.round((parseFloat(water) / parseFloat(coffee)) * 10) / 10;
+        
+    //Trigger with keydown b, n, and m. 
+    $(document).keydown(function(e){
+        if (e.which == 66){
+            roundRobinPlay(sound.boom, null, null, $('.boom'));
+        } else if (e.which == 78){
+            roundRobinPlay(sound.smack1, sound.smack2, sound.smack3, $('.smack'));
+        } else if (e.which == 77) {
+            roundRobinPlay(sound.tsst1, sound.tsst2, sound.tsst3, $('.tsst'));
+        } else {
+            console.log('ERROR');
         }
-
-        $('.answerBrew').text('1 : ' + findRatio(coffeeValue, waterValue) + ' Ratio');
-        return false;
     });
-
-    //EXTRACTION YIELD CALCULATOR
-    $('#extCalForm').submit(function(){
-        var tdsValue = $('#tdsPercent').val(),
-            dryMassValue = $('#dryMass').val(),
-            brewMassValue = $('#brewMass').val();
-
-        function findExtPercent(tds, dry, brew) {
-//            return brew * (tds / dry);
-            return Math.round((brew * (tds / dry)) * 100) / 100;
-        }
-
-        $('.answerExt').text(findExtPercent(tdsValue, dryMassValue, brewMassValue) + '%');
-        return false;
-    });
-
-    //LOSS CALCULATOR
-    $('#lossCalForm').submit(function(){
-        var preWeightValue = reconvertGrams(findUnitFromInput('#preWeight'), $('#preWeight').val()),
-            postWeightValue = reconvertGrams(findUnitFromInput('#postWeight'), $('#postWeight').val());
-
-        function findLossPercent(pre, post) {
-            return Math.round((100 - ((post / pre) * 100)) * 10) / 10;
-        }
-
-        $('.answerLoss').text(findLossPercent(preWeightValue, postWeightValue) + '%');
-        return false;
-    });
-
-    //DEV CALCULATOR
-    $('#devCalForm').submit(function(){
-        var totalMinValue = $('#totalMin').val(),
-            totalSecValue = $('#totalSec').val(),
-            devMinValue = $('#devMin').val(),
-            devSecValue = $('#devSec').val(),
-            totalTime = findSeconds(totalMinValue, totalSecValue),
-            totalDev = findSeconds(devMinValue, devSecValue);
-
-        function findSeconds (min, sec) {
-            return (parseFloat(min) * 60) + parseFloat(sec);
-        }
-
-        function findPercent (total, dev){
-            return Math.round((100 - ((dev / total) * 100)) * 10) / 10;
-        }
-
-        $('.answerDev').text(findPercent(totalTime, totalDev) + '%');
-        return false;
-    });
-
-    //TOOLTIP
-    $('.popover-div').popover();
-
-    setTimeout(function() {
-        var goat = {
-            hooves: 4,
-            eyes: 'weird',
-            sound: 'bahhh',
-            attack: 'jump kick',
-            food: ['cans', 'bananas', 'grass', 'shirts']
-        }
-    }, 10000);
-
-    var banana = 'monkey hambone dishwasher dog';
-    $('.banana').click(function(){
-        console.log('go bananas' + banana);
-    })
-
-}
-
-$(document).ready(main);
+}); 
