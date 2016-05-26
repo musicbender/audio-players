2869436508b7e911f367e604442464c4bc679c76
Pat Jacobs <patjacobs@Pats-MacBook-Air.local>
2016-05-25 22:36:03 -0700

    got it to play and stop. cannot stop currentTime or change it yet

--- js/main.js
+++ js/main.js
@@ -1,8 +1,62 @@
+
 $(document).ready(function(){
 
-    var context = new (window.AudioContext || window.webkitAudioContext)(),
-        playSound = undefined;
+    /*******Web Audio API*******/
     
+    var context = new (window.AudioContext || window.webkitAudioContext)(),
+    playSound = undefined;
+
+    function audioFileLoader(fileDirectory) {
+        var soundObj = {};
+        soundObj.fileDirectory = fileDirectory;
+        playSound = undefined;
+        var getSound = new XMLHttpRequest();
+        getSound.open("GET", soundObj.fileDirectory, true);
+        getSound.responseType = "arraybuffer";
+        getSound.onload = function() {
+            context.decodeAudioData(getSound.response, function(buffer) {
+                soundObj.soundToPlay = buffer;
+            });
+        }
+
+        getSound.send();
+
+        soundObj.play = function() {
+            playSound = context.createBufferSource();
+            playSound.buffer = soundObj.soundToPlay;
+            playSound.connect(context.destination);
+            playSound.start(context.currentTime);
+            context.resume();
+        }
+
+        soundObj.stop = function() {
+            playSound.stop(context.currentTime);
+            context.suspend();
+            console.log(context.currentTime);
+        }
+        
+        return soundObj;
+    };
+
+    //loop through list of audio files
+    function audioBatchLoader(obj) {
+        for (prop in obj) {
+            obj[prop] = audioFileLoader(obj[prop])
+        }
+        return obj
+    }
+
+    //batch audio loader
+     var sound = audioBatchLoader({
+       track1: 'audio/track1.mp3'
+     });
+
+
+    //Play sound 
+    function playGo (sound) {
+            sound.play(context.currentTime);
+    }
+
     /*******Player 1*******/
     var clickState1 = false,
         playState1 = false;
@@ -30,55 +84,16 @@ $(document).ready(function(){
             playState1 = false;
         }    
     })
-  
-    function audioFileLoader(fileDirectory) {
-        var soundObj = {};
-        soundObj.fileDirectory = fileDirectory;
-/*var*/ playSound = undefined;
-        var getSound = new XMLHttpRequest();
-        getSound.open("GET", soundObj.fileDirectory, true);
-        getSound.responseType = "arraybuffer";
-        getSound.onload = function() {
-            context.decodeAudioData(getSound.response, function(buffer) {
-                soundObj.soundToPlay = buffer;
-            });
-        }
-
-        getSound.send();
-
-        soundObj.play = function() {
-            playSound = context.createBufferSource();
-            playSound.buffer = soundObj.soundToPlay;
-            playSound.connect(context.destination)
-            playSound.start(context.currentTime)
-        }
-
-        soundObj.stop = function() {
-            playSound.stop(context.currentTime)
-        }
-        
-        return soundObj;
-    };
-
-    //loop through list of audio files
-    function audioBatchLoader(obj) {
-        for (prop in obj) {
-            obj[prop] = audioFileLoader(obj[prop])
-        }
-        return obj
-    }
-
-    //batch audio loader
-     var sound = audioBatchLoader({
-       track1: 'audio/track1.mp3'
-     });
-
-
-    //Play sound 
-    function playNow (sound) {
-            sound.play(context.currentTime);
-    }
     
     /*-----Trigger sounds-----*/
     
+    $('.ti-control-play').on('click', function(e) {
+        e.preventDefault();
+        sound.track1.play(context.currentTime);
+    });
+    $('.ti-control-pause').on('click', function(e) {
+        e.preventDefault();
+        sound.track1.stop();
+    })
+     
 }); 
